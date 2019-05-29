c7a35a8f026210f45077786ec317ae26c0f3f21e
Kayla <6lackmacaroon@gmail.com>
2019-05-29 13:30:44 -0600

    logger middleware working

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  const d = new Date();
+  const n = d.toJSON();
+  console.log(`A ${req.method} request to '${req.originalUrl}' Timestamp: ${n}`)
 };
 
 module.exports = server;
